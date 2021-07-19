de7b8d28915421d133eeced5ef71caad296584c3
jakub-siwiec <jakub.siwiec@svcais.com>
2021-07-19 23:26:52 +0100

    Created table with templates

--- email-sender/main.py
+++ email-sender/main.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
+CORS(app)
+db = SQLAlchemy(app)
+
+class EmailTemplate(db.Model):
+    __tablename__ = 'email-templates'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=True, nullable=False)
+    subject = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    db.UniqueConstraint(subject, body)
+
 
 @app.route('/')
 def index():
--- email-sender/startdev.sh
+++ email-sender/startdev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f docker-compose.dev up
+docker-compose -f docker-compose.dev.yml up
