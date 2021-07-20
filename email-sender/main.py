f1b43ea982c6ffb7a5b1e04b1a515e6ee56cd754
jakub-siwiec <jakub.siwiec@svcais.com>
2021-07-20 02:14:37 +0100

    Integrated db

--- email-sender/main.py
+++ email-sender/main.py
@@ -1,25 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@db/main'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:root@mysql-email_sender:3306/main'
 CORS(app)
 db = SQLAlchemy(app)
 
 class EmailTemplate(db.Model):
-    __tablename__ = 'email-templates'
+    __tablename__ = 'emailTemplates'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, unique=True, nullable=False)
-    subject = db.Column(db.Text, nullable=False)
-    body = db.Column(db.Text, nullable=False)
+    title = db.Column(db.String(254), unique=True, nullable=False)
+    subject = db.Column(db.String(254), nullable=False)
+    body = db.Column(db.String(1000), nullable=False)
     db.UniqueConstraint(subject, body)
+
+db.create_all()
+
+# test = EmailTemplate(title="Test", subject="Test Subject", body="Test body")
+# db.session.add(test)
+# db.session.commit()
 
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('temp-index.html', name='Test')
+
+@app.route('/email-templates')
+def templates():
+    return 'Have fun'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
